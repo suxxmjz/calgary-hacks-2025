a6d6e3809f6a755e08214321e0942a224fecee61
KirtanKakadiya <kirtan.kakadiya@gmail.com>
2025-02-15 14:30:33 -0700

    refactoring folders

---
+++ model/Animal-Classification.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix , classification_report
+import numpy as np
+import os
+
+directory = "images"
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
