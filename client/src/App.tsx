b5c022e1ffaf3825406d5f9554cf1f4882b49ce5
Manraj Singh <manrajs2025@gmail.com>
2025-02-16 15:27:26 -0700

    Refactor routes, properly install tailwindcss, install shadcn components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,14 @@ import Profile from "./pages/Profile";
 import Post from "./pages/Post";
 import BottomNav from "./components/BottomNav";
 import CommunityPage from "./pages/CommunityPage";
+import {
+  COMMUNITY_POSTS_ROUTE,
+  CREATE_POST_ROUTE,
+  LOGIN_ROUTE,
+  PROFILE_ROUTE,
+  REGISTER_ROUTE,
+} from "./utils/routes";
+import LoginPage from "./pages/LoginPage";
 
 export const BASE_API_URL = `${import.meta.env.VITE_BACKEND_API_URL}/api`;
 
@@ -21,9 +29,11 @@ function App() {
       <div className="flex-grow overflow-auto">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/communityposts" element={<CommunityPage />} />
+          <Route path={PROFILE_ROUTE} element={<Profile />} />
+          <Route path={CREATE_POST_ROUTE} element={<Post />} />
+          <Route path={COMMUNITY_POSTS_ROUTE} element={<CommunityPage />} />
+          <Route path={LOGIN_ROUTE} element={<LoginPage />} />
+          <Route path={REGISTER_ROUTE} element={<>Register Page</>} />
         </Routes>
       </div>
       <BottomNav />
---
+++ client/src/utils/routes.ts
@@ -0,0 +1,5 @@
+export const LOGIN_ROUTE = "/login";
+export const REGISTER_ROUTE = "/register";
+export const PROFILE_ROUTE = "/profile";
+export const CREATE_POST_ROUTE = "/post";
+export const COMMUNITY_POSTS_ROUTE = "/community-posts";
