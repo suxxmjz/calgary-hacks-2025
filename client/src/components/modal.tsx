b3dfd5a733259bc9873ff2c39b5709894fdbcd70
Manraj Singh <manrajs2025@gmail.com>
2025-02-19 01:25:12 -0700

    Modal tweak

--- client/src/components/modal.tsx
+++ client/src/components/modal.tsx
@@ -16,6 +16,7 @@ interface ModalProps {
   readonly showCloseButton?: boolean;
   readonly width?: string;
   readonly minWidth?: string;
+  readonly isContentLoading?: boolean;
 }
 
 export function Modal({
@@ -26,6 +27,7 @@ export function Modal({
   onClose,
   width = "w-[90%] md:max-w-lg",
   minWidth,
+  isContentLoading = false,
 }: ModalProps): JSX.Element {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -40,11 +42,21 @@ export function Modal({
             {subHeader}
           </DialogDescription>
         </DialogHeader>
-        {content && (
-          <div className="mb-2 mt-2 overflow-y-auto max-h-[94vh]">
-            {content}
-          </div>
-        )}
+        <div className="relative mb-2 mt-2 overflow-y-auto max-h-[94vh]">
+          {content && (
+            <div
+              className={`${isContentLoading ? "opacity-50" : "opacity-100"}`}
+            >
+              {content}
+            </div>
+          )}
+          {isContentLoading && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              {/* Spinner element */}
+              <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-b-4 border-teal-600" />
+            </div>
+          )}
+        </div>
       </DialogContent>
     </Dialog>
   );
