1f5a796d150329955abd24fa332551a27dc9e20c
Manraj Singh <manrajs2025@gmail.com>
2025-02-19 19:08:06 -0700

    Update seed script

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,90 +1,82 @@
 import "dotenv/config";
+import { faker } from "@faker-js/faker";
 import { createDbClient } from "./db";
 import { usersTable, badgesTable, postsTable, upvotesTable } from "./schema";
+
+const MOCK_BADGE_IMAGES = [
+  "https://static.vecteezy.com/system/resources/previews/048/687/322/non_2x/trophy-award-with-stars-in-pixel-art-style-vector.jpg",
+  "https://img.freepik.com/premium-vector/trophy-pixel-vector_1050334-10.jpg",
+  "https://static.vecteezy.com/system/resources/previews/010/967/102/non_2x/medal-pixel-icon-free-vector.jpg",
+];
 
 const dbClient = createDbClient(process.env.DEV_DATABASE_URL ?? "");
 
 async function seedDatabase() {
   console.log("Seeding database...");
 
-  // Password hashes to "pass"
-  await dbClient.insert(usersTable).values([
-    {
-      name: "Alice",
-      email: "alice@mail.com",
-      password: "$2b$10$aOs9LvrSspdABIJbhOUjN.b/F1OCUp6GZvFW2cACj89L8esTE6TN2",
-    },
-    {
-      name: "Bob Carter",
-      email: "bob@mail.com",
-      password: "$2b$10$aOs9LvrSspdABIJbhOUjN.b/F1OCUp6GZvFW2cACj89L8esTE6TN2",
-    },
-    {
-      name: "Jane Smith",
-      email: "Jane@mail.com",
-      password: "$2b$10$aOs9LvrSspdABIJbhOUjN.b/F1OCUp6GZvFW2cACj89L8esTE6TN2",
-    },
-  ]);
+  for (let idx = 0; idx < 4; idx++) {
+    const person = faker.person;
+    const fullName = idx === 1 ? "Clark Kent" : person.fullName();
+    const email =
+      idx === 1
+        ? "test@mail.com"
+        : faker.internet.email({
+            firstName: fullName.split(" ")[0],
+            lastName: fullName.split(" ")[1],
+          });
 
-  await dbClient.insert(badgesTable).values([
-    {
-      id: 1,
-      userId: 1,
-      title: "Wildlife Explorer",
-      description: "Awarded for spotting 10 different animals",
-      imageUrl: "https://picsum.photos/200",
-    },
-    {
-      id: 2,
-      userId: 2,
-      title: "Wildlife Explorer 2",
-      description: "Awarded for spotting 20 different animals",
-      imageUrl: "https://picsum.photos/200",
-    },
-    {
-      id: 3,
-      userId: 3,
-      title: "Nature Protector",
-      description: "Awarded for reporting conservation issues",
-      imageUrl: "https://picsum.photos/200",
-    },
-  ]);
+    const userInsert = await dbClient
+      .insert(usersTable)
+      .values({
+        name: fullName,
+        email,
+        password:
+          idx === 1
+            ? "$2a$10$qTwB1uJBIrlT53RmP6KDdOY4ds56Dalr6.BLOFIo3wBOfvFyeE81a" // Password is: "pass"
+            : faker.internet.password(),
+      })
+      .returning();
 
-  await dbClient.insert(postsTable).values([
-    {
-      userId: 1,
-      animal: "Lion",
-      notes: "Saw a lion resting under a tree",
-      conservationNotes: "Lions are facing habitat loss.",
-      imageUrl: "https://picsum.photos/200",
-      latitude: -1.2921,
-      longitude: 36.8219,
-    },
-    {
-      userId: 2,
-      animal: "Tiger",
-      notes: "Look at this!",
-      conservationNotes: "Rawr",
-      imageUrl: "https://picsum.photos/200",
-      latitude: -16.921,
-      longitude: 38.89,
-    },
-    {
-      userId: 3,
-      animal: "Elephant",
-      notes: "A group of elephants was walking",
-      conservationNotes: "Poaching is a serious threat to elephants.",
-      imageUrl: "https://picsum.photos/200",
-      latitude: -1.405,
-      longitude: 36.95,
-    },
-  ]);
+    const user = userInsert[0];
 
-  await dbClient.insert(upvotesTable).values([
-    { postId: 1, userId: 1 },
-    { postId: 2, userId: 2 },
-    { postId: 2, userId: 3 },
-  ]);
+    for (let badgeIdx = 0; badgeIdx < 3; badgeIdx++) {
+      await dbClient.insert(badgesTable).values({
+        id: badgeIdx + user.id,
+        userId: user.id,
+        title: `Badge #${badgeIdx + 1}`,
+        description: `Nice job! You got a badge for ${badgeIdx + 1}!`,
+        imageUrl: MOCK_BADGE_IMAGES[badgeIdx],
+      });
+    }
+
+    for (let postIdx = 1; postIdx < 4; postIdx++) {
+      const type = faker.animal.type();
+
+      const postInsert = await dbClient
+        .insert(postsTable)
+        .values({
+          userId: user.id,
+          animal: type,
+          notes: `I saw this cool ${type} today in the park!`,
+          conservationNotes: faker.lorem.sentences(3),
+          imageUrl: faker.image.urlPicsumPhotos({ width: 400, height: 400 }),
+          latitude: faker.location.latitude(),
+          longitude: faker.location.longitude(),
+          createdAt: faker.date.recent({ days: 30 }),
+        })
+        .returning();
+
+      const post = postInsert[0];
+
+      const shouldUpvote = faker.datatype.boolean();
+      if (shouldUpvote) {
+        await dbClient.insert(upvotesTable).values({
+          postId: post.id,
+          userId: user.id,
+        });
+      }
+    }
+  }
 
   console.log("Database seeded successfully!");
 }
